fbfe2739de1f8958f4efa3bbd42f04b2217d066c
Ezra Finones <e.finones@klab.com>
2015-02-23 11:24:03 +0800

    Included all chapter1 *.php files

---
+++ chapter1/hello_world.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Look Out World</title>
+</head>
+<body>
+<?php echo "Hello, world!"; ?>
+<html>
+<head>
+<title>Look Out World</title>
+</head>
+<body>
+<?php echo "Hello, world!"; ?>
+</body>
+</html>
