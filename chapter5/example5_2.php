68617226d0f5f98bf3ccfbb026229b1189f384b9
Ezra Finones <e.finones@klab.com>
2015-02-25 14:56:29 +0800

    Modified example5_2.php file

--- chapter5/example5_2.php
+++ chapter5/example5_2.php
@@ -11,15 +11,17 @@ function hasRequired($array, $requiredFields)
 	return true;
 }
 
-if ($_POST['submitted']) {
+if (isset($_POST['submitted'])) { //
 	echo "<p>You ";
 	echo hasRequired($_POST, array('name', 'email_address')) ? "did" : "did not";
 	echo " have all the required fields.</p>";
 } ?>
 
+
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 	<p>Name: <input type="text" name="name" /><br />
 	Email address: <input type="text" name="email_address" /><br />
-	Age (optional): <input type="text" name="age" /></p>
+	Age (optional): <input type="text" name="age" /></p> 
+	
 	<p align="center"><input type="submit" value="submit" name="submitted" /></p>
 </form>
