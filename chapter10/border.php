5bb46a3fcb7cd8f22c57b2b41e95df8e34b2c287
Ezra Finones <e.finones@klab.com>
2015-02-26 11:55:19 +0800

    Included all chapter 10 *.php files

---
+++ chapter10/border.php
@@ -0,0 +1,13 @@
+<?php
+require('fpdf/fpdf.php'); // path to fpdf.php
+
+$pdf = new FPDF();
+$pdf->addPage();
+
+$pdf->setFont("Arial", 'B', 16);
+$pdf->cell(90, 10, "Hello Out There!", 1, 0, 'C');
+
+$pdf->output();
+
+
+
