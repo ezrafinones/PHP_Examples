283787c9b292ed70989a9006b4c2982bccea4182
Ezra Finones <e.finones@klab.com>
2015-03-02 16:40:33 +0800

    Included all chapter15 *.php files

---
+++ chapter15/json.php
@@ -0,0 +1,9 @@
+<?php
+/*$data = array(1,2,"three");
+$jsonData = json_encode($data);
+echo $jsonData;
+*/
+
+$jsonData = "[1, 2, [3, 4], \"five\"]";
+$data = json_decode($jsonData);
+print_r($data);
